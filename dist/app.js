ad92a7cc8e15e046a93ebe22037da87469d80262
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-03-28 16:41:44 +0200

    terminando

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,19 @@
 "use strict";
 console.clear();
-//Dado un array de string, sacar los valores que estan formados por mas de dos palabras:
-const main = (valores) => {
-    console.log(valores.filter((x) => { if (x.split(' ').length !== 1) {
-        return true;
-    }
-    else {
-        return false;
-    } }));
+//Dado un array de arrays con alumnos y notas, mostrar una lista con los alumnos aprobados y suspensos.
+const main = (alumnos) => {
+    let aprobados = "\nLista de aprobados:\n";
+    let suspensos = "\nLista de suspensos:\n";
+    alumnos.forEach((x) => {
+        if (x[1] >= 5) {
+            aprobados += `${x[0]} [${x[1]}]\n`;
+        }
+        else {
+            suspensos += `${x[0]} [${x[1]}]\n`;
+        }
+        ;
+    });
+    console.log(`${aprobados}${suspensos}`);
 };
-main(['Córdoba', 'Badajoz', 'Ciudad Real', 'Barcelona', 'San Cougat']);
+main([['Alfredo', 9], ['Arturo', 3], ['Soraya', 7], ['Eugenia', 6], ['Alberto', 9], ['Pepe', 2]]);
 //# sourceMappingURL=app.js.map
