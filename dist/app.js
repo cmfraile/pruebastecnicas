1e625271bc69578fab8ee626f82145839e761a10
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-03-28 13:33:14 +0200

    equisde

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,40 @@
 "use strict";
 console.clear();
-//Realizar un reloj:
-const main = () => {
-    setInterval(() => {
-        console.clear();
-        console.log(Date().split(' ')[4]);
-    }, 1000);
+//Dado un array o texto, devolver el elemento que mas aparece:
+const main = (elemento) => {
+    let top = "";
+    let contador = 0;
+    switch (typeof (elemento)) {
+        case 'string':
+            elemento.split(' ').forEach((x) => {
+                var _a;
+                let casos = ((_a = elemento.match(RegExp(x, 'gi'))) === null || _a === void 0 ? void 0 : _a.length) || 0;
+                if (casos > contador) {
+                    top = x;
+                    contador = casos;
+                }
+            });
+            console.log(`El término mas empleado en la cadena es [${top}], usado en [${contador}] ocasiones.`);
+            ;
+            break;
+        default:
+            let cuenta = {};
+            elemento.forEach((x) => { if (!cuenta[x]) {
+                cuenta[x] = 1;
+            }
+            else {
+                cuenta[x]++;
+            } });
+            for (let x in cuenta) {
+                if (cuenta[x] > contador) {
+                    top = x;
+                    contador = cuenta[x];
+                }
+            }
+            console.log(`El dato mas empleado en el array es [${top}], usando en [${contador}] ocasiones.`);
+            ;
+            break;
+    }
 };
-main();
+main([12, 12, 12, 12, 'mamawebo']);
 //# sourceMappingURL=app.js.map
