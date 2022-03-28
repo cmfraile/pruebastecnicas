1d5bd99743898967d0c46d46ad04d02efaf4d059
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-03-28 12:22:35 +0200

    achuche

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,11 @@
 "use strict";
 console.clear();
-//Dado un array de numeros y cadenas, devolver el array sin números duplicados ni cadenas:
-const main = (array) => {
-    let primerfiltro = array.filter((x) => { if (typeof (x) == 'number') {
-        return true;
-    }
-    else {
-        return false;
-    } });
-    primerfiltro.map((x) => {
-        console.log(x, primerfiltro.join(','));
-    });
-    console.log(primerfiltro);
+//Realizar un reloj:
+const main = () => {
+    setInterval(() => {
+        console.clear();
+        console.log(Date().split(' ')[4]);
+    }, 1000);
 };
-main([12, 13, 14, 15, 15, 15, 12, 'webo']);
+main();
 //# sourceMappingURL=app.js.map
